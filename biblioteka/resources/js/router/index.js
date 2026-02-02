0a77c3333af3128b6b03421f31507db13f5da597
23DP4MMisk <A230427MM@rvt.lv>
2026-02-02 21:11:59 +0200

    pieslegsanas lapuse

--- biblioteka/resources/js/router/index.js
+++ biblioteka/resources/js/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import LibraryPage from '../pages/Library.vue';
+import LoginPage from '../pages/PieslLibrary.vue'
 
 const routes = [
     { 
@@ -12,7 +13,13 @@ const routes = [
        path: '/library',
        name: 'Library',
        component: LibraryPage 
+    },
+     {
+       path: '/login',
+       name: 'PieslLibrary',
+       component: LoginPage 
     }
+
 ];
 
 const router = createRouter({
