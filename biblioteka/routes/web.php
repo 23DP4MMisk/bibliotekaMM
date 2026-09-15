2559159462f01a6df9a54ff43a440e1d8d2c6816
23DP4MMisk <A230427MM@rvt.lv>
2026-09-15 21:57:31 +0300

    css izlabošana

--- biblioteka/routes/web.php
+++ biblioteka/routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Для всех остальных маршрутов тоже возвращаем welcome
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '^(?!api).*$');
